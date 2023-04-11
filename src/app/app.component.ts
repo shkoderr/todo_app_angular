17252bb2c65335b77f5ffd7283e98c00bbd2e47e
Timur Birgalin <elecktrohachyck@gmail.com>
2023-04-11 16:44:16 +0400

    started my learning Todo app using Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo_app';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    {description: 'eat', done: true},
+    {description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
 }
